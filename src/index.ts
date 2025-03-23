b8c2fd6976e784cfac3da597e87c17b22af9d043
Faheem Akhtar <faheemakhtar19730@gmail.com>
2025-03-23 22:41:53 +0530

    port changes

--- src/index.ts
+++ src/index.ts
@@ -17,5 +17,5 @@ app.use(express.json());
 app.use('/api/auth', authRoutes)
 app.use('/api/role', roleRoutes)
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
